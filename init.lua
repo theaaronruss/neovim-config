b1acc2cd41b629c53283921530172a2b5eb78a1e
Aaron Russell <158230700+theaaronruss@users.noreply.github.com>
2026-05-18 01:22:54 -0400

    Add Telescope plugin

--- init.lua
+++ init.lua
@@ -5,14 +5,40 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 4
-vim.opt.colorcolumn = "80"
+vim.opt.colorcolumn = '80'
+
+-------------
+-- Plugins --
+-------------
+
+-- Gruvbox
+vim.pack.add({
+  'https://github.com/ellisonleao/gruvbox.nvim'
+})
+require('gruvbox').setup()
+vim.cmd.colorscheme('gruvbox')
+
+-- Plenary
+vim.pack.add({
+    'https://github.com/nvim-lua/plenary.nvim'
+})
+
+-- Telescope
+vim.pack.add({
+    'https://github.com/nvim-telescope/telescope.nvim'
+})
 
 ------------------
 -- Key Mappings --
 ------------------
 vim.g.mapleader = ' '
-vim.keymap.set('n', '<leader>e', vim.cmd.Ex)
-vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float)
+vim.keymap.set('n', '<leader>e', vim.cmd.Ex, { desc = 'Show file explorer' })
+vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, { desc = 'Show diagnostic under cursor' })
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Fuzzy find files' })
+vim.keymap.set('n', '<leader>fs', builtin.live_grep, { desc = 'Live grep' })
+vim.keymap.set('n', '<leader>ls', builtin.lsp_document_symbols, { desc = 'Search document symbols' })
+vim.keymap.set('n', '<leader>dl', builtin.diagnostics, { desc = 'Show all diagnostics for file' })
 
 ---------
 -- LSP --
@@ -24,13 +50,3 @@ vim.lsp.config['clangd'] = {
 }
 vim.lsp.enable('clangd')
 
--------------
--- Plugins --
--------------
-
--- Gruvbox
-vim.pack.add({
-  "https://github.com/ellisonleao/gruvbox.nvim"
-})
-require("gruvbox").setup()
-vim.cmd.colorscheme("gruvbox")
