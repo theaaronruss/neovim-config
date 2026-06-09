d210c3c85f9c0820cbf244f075e5702e8c4f10b0
Aaron Russell <158230700+theaaronruss@users.noreply.github.com>
2026-06-08 21:57:09 -0400

    Update config to use lazy.nvim

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,8 @@
+vim.keymap.set('n', '<leader>e', vim.cmd.Ex, { desc = 'Show file explorer' })
+vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, { desc = 'Show diagnostic under cursor' })
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Fuzzy find files' })
+vim.keymap.set('n', '<leader>fs', builtin.live_grep, { desc = 'Live grep' })
+vim.keymap.set('n', '<leader>ls', builtin.lsp_document_symbols, { desc = 'Search document symbols' })
+vim.keymap.set('n', '<leader>dl', builtin.diagnostics, { desc = 'Show all diagnostics for file' })
+
---
+++ lua/config/options.lua
@@ -0,0 +1,6 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.colorcolumn = '80'
+vim.cmd.colorscheme 'catppuccin-frappe'
