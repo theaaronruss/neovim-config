876a971896966236d5393db796b0e20b08cb3ba0
Aaron Russell <158230700+theaaronruss@users.noreply.github.com>
2026-07-29 19:56:04 -0400

    Enable local neovim configs in projects and set column to 120

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,5 +2,7 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 4
-vim.opt.colorcolumn = '80'
+vim.opt.colorcolumn = '120'
 vim.cmd.colorscheme 'catppuccin-frappe'
+vim.opt.exrc = true
+
