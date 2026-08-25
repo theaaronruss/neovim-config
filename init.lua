50234a449bffa8daada8581428c5817b74bdd734
Aaron Russell <158230700+theaaronruss@users.noreply.github.com>
2026-08-25 00:58:23 -0400

    Format Go files on write

--- init.lua
+++ init.lua
@@ -10,6 +10,14 @@ vim.g.leader = ' '
 
 vim.cmd.colorscheme('catppuccin')
 
+-- Format Go files on write
+vim.api.nvim_create_autocmd('BufWritePre', {
+    pattern = '*.go',
+    callback = function()
+        vim.lsp.buf.format({ async = false })
+    end,
+})
+
 vim.pack.add({
     'https://github.com/neovim/nvim-lspconfig',
 })
